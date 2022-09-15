0bb1b89fb25ddcf7248dc4c4adc22c4513394afd
Chris Hand <cj.hand@gmail.com>
2022-09-15 07:42:51 -0700

    More wiring

---
+++ Tipsy/ResultsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Chris Hand on 9/15/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+}
